d6259e241911e8a897c872a019ef3a531f2d5653
anuragchoudhary1910 <anuragchoudhary1910@gmail.com>
2020-11-03 01:26:06 +0530

    test

--- public/script.js
+++ public/script.js
@@ -1,75 +1,77 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
-const myVideo = document.createElement('video')
-const peerVideo = document.createElement('video')
-myVideo.muted = true
 
 const peer = new Peer(undefined, {
-  path: '/peerjs',
   host: '/',
-  port: '443',
+  port: '5000',
+  path: '/peerjs',
 })
-
-var userName = prompt('Please enter your name...')
-
-//Connects this device media to the stream
-let myVideoStream
+const myPeer = {}
+const myVideo = document.createElement('video')
+myVideo.muted = true
+// let userName = prompt('Please enter your name...')
+let mystream
 navigator.mediaDevices
-  .getUserMedia({
-    video: true,
-    audio: true,
-  })
+  .getUserMedia({ video: true, audio: true })
   .then((stream) => {
-    myVideoStream = stream
-    addVideoStream(myVideo, myVideoStream)
-  })
+    mystream = stream;
+    addVideoStream(myVideo, stream);
+    socket.on('user-connected', (anotherPeerData) => {
+      console.log('userconnected', anotherPeerData)
+      connectToNewUser(anotherPeerData.id, mystream)
+      requestHandler(anotherPeerData)
+    })
 
+    peer.on('call', (call) => {
+      call.answer(mystream)
+      console.log('call')
+      const video = document.createElement('video')
+      call.on('stream', (userVideoStream) => {
+        addVideoStream(video, userVideoStream)
+      })
+    })
+  })
+  .catch((err) => console.log(err))
+
+socket.on('user-disconnected', (userId) => {
+  if (myPeer[userId]) myPeer[userId].close()
+  console.log(userId, 'disconnect')
+})
 const addVideoStream = (video, stream) => {
   video.srcObject = stream
   video.addEventListener('loadedmetadata', () => {
     video.play()
   })
   const div = document.createElement('div')
-  const h = document.createElement('h4')
   videoGrid.append(div)
   div.append(video)
+  console.log('addvideostream')
 }
 
-//Opens connection i.e makes a call
 peer.on('open', (id) => {
-  socket.emit('join-room', { ROOM_ID: ROOM_ID, id: id, name: userName })
+  console.log(id)
+  socket.emit('join-room', { ROOM_ID: ROOM_ID, myId: id, name: 'userName' })
 })
 
-//Answer the call
-peer.on('call', (call) => {
-  call.answer(myVideoStream)
+const connectToNewUser = (id, stream) => {
+  const call = peer.call(id, stream)
   call.on('stream', (userVideoStream) => {
-      console.log(userVideoStream)
-    addVideoStream(peerVideo, userVideoStream)
-  })
-})
-
-//After user has answered the call
-socket.on('user-connected', (data) => {
-  console.log(data)
-  connectToNewUser(data, myVideoStream)
-})
-
-// socket.emit('join-room', ROOM_ID);
-
-const connectToNewUser = (data, stream) => {
-  const call = peer.call(data.id, stream)
-  const video = document.createElement('video')
-  call.on('stream', (userVideoStream) => {
+    const video = document.createElement('video')
     addVideoStream(video, userVideoStream)
+    console.log('connectetonewuswer')
   })
+
+  call.on('close', () => {
+    video.remove()
+  })
+  myPeer[id] = call
 }
 
-let text = $('input')
+let text = $('#chat__message')
 
-$('html').keydown((e) => {
+$('#chat__message').keydown((e) => {
   if (e.which == 13 && text.val().length !== 0) {
-    socket.emit('message', { name: userName, message: text.val() })
+    socket.emit('message', { name: 'userName', message: text.val() })
     text.val('')
   }
 })
@@ -77,7 +79,7 @@ $('html').keydown((e) => {
 socket.on('createMessage', (data) => {
   console.log(data)
   let addClass
-  if (data.name == userName) {
+  if (data.name == 'userName') {
     addClass = 'right'
     name = ''
   } else {
@@ -98,13 +100,13 @@ const scrollToBottom = () => {
 }
 
 const muteUnmute = () => {
-  const enabled = myVideoStream.getAudioTracks()[0].enabled
+  const enabled = mystream.getAudioTracks()[0].enabled
   if (enabled) {
-    myVideoStream.getAudioTracks()[0].enabled = false
+    mystream.getAudioTracks()[0].enabled = false
     setUnmuteButton()
   } else {
     setMuteButton()
-    myVideoStream.getAudioTracks()[0].enabled = true
+    mystream.getAudioTracks()[0].enabled = true
   }
 }
 
@@ -121,24 +123,33 @@ const setUnmuteButton = () => {
 }
 
 const videoOnOff = () => {
-  const enabled = myVideoStream.getVideoTracks()[0].enabled
+  const enabled = mystream.getVideoTracks()[0].enabled
   if (enabled) {
-    myVideoStream.getVideoTracks()[0].enabled = false
+    mystream.getVideoTracks()[0].enabled = false
     setVideoOff()
   } else {
     setVideoOn()
-    myVideoStream.getVideoTracks()[0].enabled = true
+    mystream.getVideoTracks()[0].enabled = true
   }
 }
 
 const setVideoOn = () => {
   const html = `<i class="fas fa-video"></i>
-    <span>Video ON</span>`
+    <span>ON</span>`
   document.querySelector('.main__onoff__button').innerHTML = html
 }
 
 const setVideoOff = () => {
   const html = `<i class="videoOff fas fa-video-slash"></i>
-    <span>Video OFF</span>`
+    <span>OFF</span>`
   document.querySelector('.main__onoff__button').innerHTML = html
+}
+
+const requestHandler = (data) => {
+  const newRequest = `<button class="dropdown-item" type="button">${data.name}</button>`
+  $('#main__controls__request').append(newRequest)
+}
+
+const createRoom = () => {
+  location.assign('/create-room')
 }
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// const firebase = require('firebase')
+// const axios = require('axios')
 const express = require('express')
 const app = express()
 const server = require('http').Server(app)
@@ -8,26 +10,56 @@ const peerServer = ExpressPeerServer(server, {
   debug: true,
 })
 
+// const firebaseApp = firebase.initializeApp({
+//   apiKey: 'AIzaSyCuSq6-JS-E-Qox9PN_KPDmYAHoBH0xU20',
+//   authDomain: 'face-to-face-4e231.firebaseapp.com',
+//   databaseURL: 'https://face-to-face-4e231.firebaseio.com',
+//   projectId: 'face-to-face-4e231',
+//   storageBucket: 'face-to-face-4e231.appspot.com',
+//   messagingSenderId: '469033613149',
+//   appId: '1:469033613149:web:6b397e4cfd3a6c01a31ab4',
+//   measurementId: 'G-DG3WYD0W77',
+// })
+// const db = firebaseApp.database()
+
 app.set('view engine', 'ejs')
-app.use(express.static('public'))
+//app.use(express.static('public'))
+app.use(express.static(__dirname + '/public'))
 app.use('/peerjs', peerServer)
 
 app.get('/', (req, res) => {
+  res.render('home', { roomId: 'home' })
+})
+app.get('/create-room', (req, res) => {
   res.redirect(`/${uuidv4()}`)
 })
 
 app.get('/:room', (req, res) => {
-  res.render('room', { roomId: req.params.room })
+  res.render('home', { roomId: req.params.room })
 })
 
-io.on('connection', (socket) => {
+io.on('connect', (socket) => {
   socket.on('join-room', (data) => {
+    // axios.post(
+    //   'https://face-to-face-4e231.firebaseio.com/users/' +
+    //     data.ROOM_ID +
+    //     '.json',
+    //   {
+    //     key: data.id,
+    //     username: data.name,
+    //     roomId: data.ROOM_ID,
+    //   },
+    // )
+
     socket.join(data.ROOM_ID)
-    socket.to(data.ROOM_ID).broadcast.emit('user-connected', data)
+    socket.to(data.ROOM_ID).emit('user-connected', data)
     socket.on('message', (message) => {
       io.to(data.ROOM_ID).emit('createMessage', message)
+    })
+    socket.on('disconnect', () => {
+      socket.to(data.ROOM_ID).broadcast.emit('user-disconnected', data.id)
     })
   })
 })
 
-server.listen(process.env.PORT || 3000)
+server.listen(process.env.PORT || 5000)
